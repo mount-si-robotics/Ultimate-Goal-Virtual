8548db4a5c6a80a6d2255df02997cfef1acadc83
Sei4or <sei4or@gmail.com>
2020-12-10 15:40:39 -0800

    basic autonomous

--- TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/autonomous/SimpleAutonomousPark.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/autonomous/SimpleAutonomousPark.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
-public class SimpleAutonomousPark {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Simple Autonomous Park")
+public class SimpleAutonomousPark extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumHardwareInterface hardwareInterface = new MecanumHardwareInterface(hardwareMap);
+
+        double motorTicksPerInch = (2 * Math.PI * 2) / hardwareInterface.getTicksPerRevolution();
+        int targetPosition = (int) (10 / motorTicksPerInch);
+
+        hardwareInterface.setDriveTargetPosition(targetPosition);
+        hardwareInterface.setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        hardwareInterface.setDriveMotorPower(0.2);
+
+        while (opModeIsActive() && hardwareInterface.isDriveMotorBusy()) {
+            idle();
+        }
+
+        hardwareInterface.setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
 }
