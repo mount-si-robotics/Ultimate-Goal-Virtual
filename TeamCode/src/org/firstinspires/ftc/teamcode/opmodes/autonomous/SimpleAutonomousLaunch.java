e56ff0a74754068dac9e8f9a0a31d6bea1a32bf6
Sei4or <sei4or@gmail.com>
2021-04-15 16:03:46 -0700

    temp autonomous launch solution

--- TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/autonomous/SimpleAutonomousLaunch.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/autonomous/SimpleAutonomousLaunch.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Simple Autonomous Launch")
 public class SimpleAutonomousLaunch extends LinearOpMode {
@@ -10,10 +11,16 @@ public class SimpleAutonomousLaunch extends LinearOpMode {
     @Override
     public void runOpMode() {
         hardwareInterface = new AutonomousMecanumHardwareInterface(hardwareMap);
+        hardwareInterface.shooterMotor.setPower(1.0f);
         waitForStart();
-        hardwareInterface.launch(0.8f);
+        ElapsedTime time = new ElapsedTime();
         while (opModeIsActive()) {
-
+            if (time.milliseconds() > 500) {
+                hardwareInterface.shooterTrigServo.setPosition(1.0);
+                time.reset();
+            } else {
+                hardwareInterface.shooterTrigServo.setPosition(0.0);
+            }
         }
     }
 }
